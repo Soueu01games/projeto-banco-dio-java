08fea07a99f7f7390155f63277ee32b6dbeb44fc
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-06 15:15:17 -0300

    Criei o banco

--- conta-banco/src/Banco.java
+++
@@ -1,5 +0,0 @@
-public class Banco {
-    public static void main(String[] args) throws Exception {
-        System.out.println("O banco começou!");
-    }
-}
---
+++ conta-banco/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número da conta: ");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("\nDigite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("\nDigite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("\nDigite o seu saldo: ");
+        double saldo = scanner.nextDouble();
+        scanner.close();
+        System.out.println("");
+
+        System.out.println("Olá "+ nome +" , obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+
+    }
+}
